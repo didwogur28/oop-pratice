159704445f1f9af61205053a6d4bfb26c3318262
didwogur28 <didwogur28@naver.com>
2023-05-31 15:17:22 +0900

    # 객체지향 방식으로 계산기 프로그램 실습
    객제지향 방식으로 계산기 프로그램 실습

---
+++ src/test/java/org/example/PositiveNumberTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.example.calculate.PositiveNumber;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+public class PositiveNumberTest {
+
+    @Test
+    void createTest() {
+
+        assertThatCode(() -> new PositiveNumber(0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("0또는 음수를 전달할 수 없습니다.");
+
+    }
+}
